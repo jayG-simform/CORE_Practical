27c667ef5157a1e5c9849a312696977a79ba9d51
Jay Gohel <jay.g@simformsolutions.com>
2023-07-07 17:05:26 +0530

    Added Practical-16

---
+++ Practical-16/Practical-16/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practical_16.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+        [HttpGet(Name ="GetString")]
+        public string Get()
+        {
+            logger.LogInformation("Hello World with Api");
+            return "Hello World";
+        }
+    }
+}
